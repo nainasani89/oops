0b3f16c79199e83f34fa7833a39704bc3170a08d
52084713 <52084713@METBLR52084713.ACSMET.COM>
2020-01-12 19:28:37 +0530

    could help me how can i calculate total marks and print in "totalMarks()"

---
+++ src/day5/InterfaceDemo1.java
@@ -0,0 +1,10 @@
+package day5;
+
+public interface InterfaceDemo1 {
+
+	 int sportsMarks( int value1);
+	 int  culturalMarks( int value2);
+	 int academicMarks( int value3);
+    int totalMarks( int Smarks, int cMarks, int aMarks);
+
+}
---
+++ src/day5/college1.java
@@ -0,0 +1,48 @@
+package day5;
+
+public class college1 implements InterfaceDemo1 {
+
+	
+	public static void main(String[] args) {
+		college1 c = new college1();
+		 c.academicMarks(20);
+		 c.culturalMarks(45);
+		 c.sportsMarks(10);
+		 c.totalMarks(10, 20, 45);
+	
+	}
+
+	
+	public int sportsMarks( int value1) {
+		
+	  int marks1 = value1;
+		
+		System.out.println( "marks in sports is" +marks1 );
+		return marks1;
+		
+	}
+
+	
+	public int culturalMarks( int value2) {
+		 int marks2 = value2;
+		System.out.println("marks in cultural"+marks2);
+		
+		return marks2;
+	}
+
+	
+	public int academicMarks( int value3) {
+		int marks3 = value3;
+		System.out.println("marks in academics" +marks3);
+		
+		return marks3;
+	}
+
+	
+	public int totalMarks(int sMarks, int cMarks, int aMarks ) {
+		return aMarks;
+		
+		
+	
+	}
+}
